b162e503fc0cab6aa0921166501c84e498b23710
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2021-06-06 13:39:01 +0300

    remove vibration on playing multiple notes together

--- src/utils/sound.utils.js
+++ src/utils/sound.utils.js
@@ -2,9 +2,13 @@ import { firthOctave } from 'constants/frequency';
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
+const gainNode = audioContext.createGain();
+gainNode.gain.value = 1 / 12;
+gainNode.connect(audioContext.destination);
+
 export function createOscillator(note) {
   const oscillator = audioContext.createOscillator();
-  oscillator.connect(audioContext.destination);
+  oscillator.connect(gainNode);
   oscillator.frequency.value = firthOctave[note];
   return oscillator;
 }
